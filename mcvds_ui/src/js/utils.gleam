726a734e6bece1872200bb3d99cec80b07fbf9ee
Matti Eiden <snaipperi@gmail.com>
2024-11-18 22:28:34 +0200

    feat: add localStorage utility functions

--- mcvds_ui/src/js/utils.gleam
+++ mcvds_ui/src/js/utils.gleam
@@ -1,2 +1,10 @@
-@external(javascript, "./js/utils.mjs", "origin")
+//import gleam/option.{type Option}
+
+@external(javascript, "./utils.mts", "origin")
 pub fn origin() -> String
+
+@external(javascript, "./utils.mts", "localStorageSetItem")
+pub fn local_storage_set_item(key: String, value: String) -> Nil
+
+@external(javascript, "./utils.mts", "localStorageGetItem")
+pub fn local_storage_get_item(key: String) -> Result(String, Nil)
--- mcvds_ui/src/js/utils.mjs
+++ mcvds_ui/src/js/utils.mjs
@@ -1,3 +1,26 @@
+import { Ok, Error } from "../gleam.mjs";
+
 export function origin() {
   return window.location.origin;
+}
+
+export function localStorageSetItem(key, value) {
+  try {
+    localStorage.setItem(key, value);
+  } catch (e) {
+    console.error("Failed to localStorage.setItem", e);
+  }
+}
+
+export function localStorageGetItem(key) {
+  try {
+    const value = localStorage.getItem(key);
+    if (value) {
+      return new Ok(value)
+    }
+    return new Error(null);
+  } catch (e) {
+    console.error("Failed to localStorage.getItem", e);
+    return new Error(null);
+  }
 }
